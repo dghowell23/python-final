8c8fb8a8ddb80d365fb264ce46649c08b30c4b29
dghowell23 <david.howell91@gmail.com>
2017-05-09 16:43:39 -0400

    Add files via upload

--- final_math_1.py
+++ final_math_1.py
@@ -3,7 +3,7 @@
 import math # required to use the math modules
 total = 0 #set the accumulator for total to zero 
 another = 'y' #set the variable another = y for the while loop
-count = 0 #set the count accumulator to zero
+count = 1 #set the count accumulator to zero
 while another == 'y' or another == 'Y':
     #have the user enter a test score
     score = float(input('Enter the score for a test: '))
@@ -12,10 +12,17 @@ while another == 'y' or another == 'Y':
     #ask the user if they have another score to add in
     another = input('Would you like to add another score? ' + \
                     '(Enter y or Y for yes):')
+    if float(total % count < .5):
+        avg = math.floor(total / count)
+    elif float(total % count >= .5):
+        avg = math.ceil(total / count)
+    else:
+        print('There is an error in your entries somewhere.  Please try again')
     #if else statement for the result of the while loop
     if another == 'y' or another == 'Y':
         count = count + 1
     else:
         print('The total of the scores is: ', format(total, '.2f'))
         print('The actual average of your scores is: ', format((total / count), '.2f'))
-        print('The average of your scores(rounded up) is: ', (math.ceil(total / count)))
+        print('The average of your scores(rounded) is: ', avg)
+
